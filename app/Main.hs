c35568c8d8f939db9ba99a9e74c0f2c52044c6e8
Richard T. Weeks <rtweeks2@illinois.edu>
2019-02-24 06:03:15 -0800

    Get program running
    At this point, it can only read a file and not accept YAML to parse on
    the command line.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
+import qualified Data.ByteString.Lazy as BS
+import System.Environment (getArgs, lookupEnv)
+import Text.Yaml.Reference (yaml)
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    [filePath] <- getArgs
+    content <- BS.readFile filePath
+    let (output, _) = normalize (yaml filePath content False)
+    putStr output
